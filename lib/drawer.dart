62dab5126a03a94a8b35eca4b4fe8ef2c04125aa
Anil Patel <anil.shayona@gmail.com>
2021-08-15 15:51:59 +0530

    chapter 3 completed

---
+++ lib/drawer.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+              accountName: Text("Anil Patel"),
+              accountEmail: Text("abc@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"),
+              )),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Account"),
+            subtitle: Text("Personal"),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("Email"),
+            subtitle: Text("abc@gmail.com"),
+            trailing: Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/pages/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Awesome App",
     home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.black,
-            width: 116,
-            height: 316,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.orange,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.white,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.green,
-                    width: 100,
-                    height: 100,
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
